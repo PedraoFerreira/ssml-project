3c9f6d6f078da25a62454a65bf74ebdae4d02f85
Pedro Ferreira da Silva Junior <pedrof.jr@gmail.com>
2019-10-17 02:48:50 -0300

    ajuste nome arquivo de download

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -10,28 +10,42 @@ def index():
     return render_template("index.html")
 
 
-def exec_ml(file_name, pred_col, model_type):
+def detect_delimiter(csv_file):
+    with open(csv_file, 'r') as file:
+        header = file.readline()
+        if header.find(";") != -1:
+            return ";"
+        if header.find(",") != -1:
+            return ","
+    return ";"
+
+def exec_ml(file_name, pred_col, model_type, extension, delimiter):
     body = {
         "s3bucket": {
-            "origin": "user-data-upload",
-            "output": "user-data-upload-output"
+            "origin": app.config['S3_USER_DATA_BUCKET_INPUT'],
+            "output": app.config['S3_USER_DATA_BUCKET_OUTPUT']
         },
         "file": {
             "name": file_name,
-            "extension": "csv",
-            "delimiter": ","
+            "extension": extension,
+            "delimiter": delimiter
         },
         "pred_col": pred_col,
         "model_type": model_type
     }
     result = req.post(app.config['URL_LAMBDA_ML'], json=body)
 
-    return result.status_code == 200
+    score = 0.0
+
+    if result.status_code == 200:
+        score = result.json()['Score'] * 100
+
+    return result.status_code == 200, score
+
 
 @app.route("/upload", methods=['POST'])
 def upload():
     target = os.path.join(APP_ROOT, '.temp')
-    print(target)
 
     if not os.path.isdir(target):
         os.mkdir(target)
@@ -40,7 +54,9 @@ def upload():
     s3_client = session.client('s3')
 
     for file in request.files.getlist("file"):
-        filename = str(uuid.uuid4()) + os.path.splitext(file.filename)[1]
+        processed_filename = 'ssml_{}'.format(file.filename)
+        extension = os.path.splitext(file.filename)[1].replace('.','')
+        filename = '{}.{}'.format(str(uuid.uuid4()),extension)
         destination = "/".join([target, filename])
 
     model_type = request.form['modeltype']
@@ -49,14 +65,17 @@ def upload():
     if filename:
         try:
             file.save(destination)
-            s3_client.upload_file(destination, 'user-data-upload', filename)
+            delimiter = detect_delimiter(destination)
+            s3_client.upload_file(destination, app.config['S3_USER_DATA_BUCKET_INPUT'], filename)
         finally:
             os.remove(destination)
 
-        if exec_ml(filename, pred_col, model_type):
+        success, accuracy_score = exec_ml(filename, pred_col, model_type, extension, delimiter)
+
+        if success:
             try:
-                s3_client.download_file('user-data-upload-output', filename, destination)
-                return send_from_directory(directory=target, filename=filename)
+                s3_client.download_file(app.config['S3_USER_DATA_BUCKET_OUTPUT'], filename, destination)
+                return send_from_directory(directory=target, filename=filename, as_attachment=True, attachment_filename=processed_filename)
             finally:
                 os.remove(destination)
 
--- config.py
+++ config.py
@@ -8,6 +8,8 @@ class Development(Config):
     PORT = 4555
     SQLALCHEMY_DATABASE_URI = 'sqlite:///storage.db'
     URL_LAMBDA_ML = 'https://5md75t1y4a.execute-api.us-east-1.amazonaws.com/PROD'
+    S3_USER_DATA_BUCKET_INPUT = 'user-data-upload'
+    S3_USER_DATA_BUCKET_OUTPUT = 'user-data-upload-output'
 
 class Testing(Config):
     TESTING = True
@@ -15,3 +17,5 @@ class Testing(Config):
 class Production(Config):
     SQLALCHEMY_DATABASE_URI = 'sqlite:///storage.db'
     URL_LAMBDA_ML = 'https://5md75t1y4a.execute-api.us-east-1.amazonaws.com/PROD'
+    S3_USER_DATA_BUCKET_INPUT = 'user-data-upload'
+    S3_USER_DATA_BUCKET_OUTPUT = 'user-data-upload-output'
